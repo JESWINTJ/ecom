55e203de39af9cccbf3742f29d5a6ba1d7242de2
minipprojetc5202 <jeswintjcherpu@gmail.com>
2025-07-28 23:57:10 +0530

    fix

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -17,10 +17,12 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(mainApiRouter); // ✅ This must be present
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
+app.use(cors());
+
 
 // Routes
 app.use('/api', router);
