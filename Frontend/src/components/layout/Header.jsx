e9b8e87211eb46e6ea8081e4b8b2e1e2fde8aee6
JESWINTJ <146103442+JESWINTJ@users.noreply.github.com>
2025-09-05 18:41:59 +0530

    Update Header.jsx

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
 
         <Link to="/Userprofile" className="text-gray-700 hover:text-blue-600 rounded-full p-2">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2H7a2 2 0 00-2 2v2m7-7V3" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A9 9 0 0112 15a9 9 0 016.879 2.804M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
         </Link>
          <Link to="/" className="text-gray-700 hover:text-blue-600 rounded-full p-2">
