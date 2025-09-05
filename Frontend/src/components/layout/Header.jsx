588912129df4f7f3abdf7383492ad19e35866c70
JESWINTJ <146103442+JESWINTJ@users.noreply.github.com>
2025-09-05 18:43:08 +0530

    Update Header.jsx

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
       <div className="flex items-center space-x-4">
         <Link to="/login" className="flex items-center text-gray-700 hover:text-blue-600 rounded-full p-2">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-7.5A2.25 2.25 0 003.75 5.25v13.5A2.25 2.25 0 006 21h7.5a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
           </svg>
           <span className="ml-1 hidden sm:inline">login</span>
         </Link>
